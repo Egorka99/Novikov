34aeccc9f537a5e79beab26db284228b7513d528
Егор <egorzavgorodnev@gmail.com>
2019-11-20 11:32:10 +0300

    create project

---
+++ wordsearch/src/main/java/com/novikov/WordSearch.java
@@ -0,0 +1,12 @@
+package com.novikov;
+
+public class WordSearch {
+
+    private int[][] inputField;
+
+    public WordSearch(int[][] inputField) {
+        this.inputField = inputField;
+    }
+
+    
+}
